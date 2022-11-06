3ba4838b0396b9844d73ef278f66079cdc0752bc
AjikK <kristiyawan16@gmail.com>
2022-11-06 19:25:20 +0700

    first commit

---
+++ kucing.js
@@ -0,0 +1,64 @@
+//element
+const popcat = document.querySelector(".popcat");
+const btn = document.querySelector(".btn");
+const point = document.getElementById("point");
+const text = document.getElementById("saka");
+
+//img
+const openImg = "open.png";
+const closeImg = "close.png";
+//sound
+const openSound = new Audio("soundOpen.mp3");
+const closeSound = new Audio("soundClose.mp3");
+const sakanigadik = new Audio("sakanigadik.mp3");
+const bakso = new Audio("bakso.mp3");
+//sakanigadik.play();
+//
+
+// Event Handlers (Desktops)
+btn.addEventListener("mousedown", openMouth);
+btn.addEventListener("mouseup", closeMouth);
+
+// Event Handers (Touch Screens)
+btn.addEventListener("touchstart", function (e) {
+  e.preventDefault();
+  openMouth();
+});
+
+btn.addEventListener("touchend", function (e) {
+  e.preventDefault();
+  closeMouth();
+});
+
+// The functions which will perform the cool stuff
+let score = 0;
+let nilais = 0;
+let nilaib = 0;
+function openMouth() {
+  popcat.src = openImg;
+  openSound.play();
+  score++;
+  point.innerHTML = score;
+
+  //shake animation
+  point.classList.toggle("move");
+
+  //sakanigedik play jika score berkelipatan 100
+
+  nilais++;
+  nilaib++;
+  if (nilais == 1000) {
+    sakanigadik.play();
+    nilais -= 1000;
+    saka.innerHTML = "anda sangat gabut sakanigadiiiikkkk!!!!!!!!!!!";
+  } else if (nilaib == 100) {
+    bakso.play();
+    nilaib -= 100;
+  }
+}
+
+function closeMouth() {
+  popcat.src = closeImg;
+  closeSound.play();
+  point.classList.toggle("move");
+}
